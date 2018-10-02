26b12ca3eed4d10fdfc42c0b30be623ec3efbf70
Quan Vuong <quan.vuong@hotschedules.com>
2018-10-02 17:03:02 +0700

    change wrong place

--- iac/terraform/s3/example_s3.tf
+++ iac/terraform/s3/example_s3.tf
@@ -15,12 +15,12 @@ resource "aws_s3_bucket" "s3-example" {
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+  # Tells Terraform that this EC2 instance must be created only after the
+  # S3 bucket has been created.
+  depends_on = ["aws_s3_bucket.s3-example"]
+}
 
 resource "aws_instance" "another" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
-}
-  # Tells Terraform that this EC2 instance must be created only after the
-  # S3 bucket has been created.
-  depends_on = ["aws_s3_bucket.s3-example"]
 }
