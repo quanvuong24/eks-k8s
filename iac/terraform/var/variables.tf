dabad571b8779aeeee9a1123fe77d578c753f68d
Quan Vuong <quan.vuong@hotschedules.com>
2018-10-08 13:53:55 +0700

    edit with maps ami

--- iac/terraform/var/variables.tf
+++ iac/terraform/var/variables.tf
@@ -1,3 +1,4 @@
+/*
 variable "access_key" {}
 variable "secret_key" {}
 variable "region" {
@@ -8,4 +9,22 @@ provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
+}
+*/
+variable "region" {}
+variable "amis" {
+  type = "map"
+  default = {
+    "us-east-1" = "ami-b374d5a5"
+    "us-west-2" = "ami-4b32be2b"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "${lookup(var.amis, var.region)}"
+  instance_type = "t2.micro"
+}
+
+output "ami" {
+  value = "${lookup(var.amis, var.region)}"
 }
