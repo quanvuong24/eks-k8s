86af65826a161eb112f571023a0ca6603de821f5
Quan Vuong <quan.vuong@hotschedules.com>
2018-10-08 13:57:40 +0700

    add provider

--- iac/terraform/var/variables.tf
+++ iac/terraform/var/variables.tf
@@ -4,13 +4,13 @@ variable "secret_key" {}
 variable "region" {
   default = "us-east-1"
 }
-
+*/
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
 }
-*/
+
 variable "region" {}
 variable "amis" {
   type = "map"
