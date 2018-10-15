a5b7532caff43a9d72d7f835e7a51d3550295350
Quan Vuong <quan.vuong@hotschedules.com>
2018-10-15 13:50:30 +0700

    test ec2 modules

---
+++ iac/terraform/ec2/ec2-instance.tf
@@ -0,0 +1,18 @@
+module "ec2_cluster" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name           = "my-cluster"
+  instance_count = 2
+
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "EC2Keypair"
+  monitoring             = true
+  vpc_security_group_ids = ["MyEC2DMZ"]
+  subnet_id              = "subnet-ce04b8e1"
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
